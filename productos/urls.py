63f02eb31aac4ed9ceb562f59dc3c792bf52a56b
evaestefhanybarreraperez@gmail.com <pedrofernandezvalera98@gmail.com>
2025-06-13 20:31:56 -0500

    listo_para_transformers

--- productos/urls.py
+++ productos/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("productos/index/", index_productos, name="index_productos")
+    path("productos/index/", index_productos, name="index_productos"),
+    path("productos/nuevo", registrar_productos, name="nuevo_productos"),
+    path("productos/editar/<int:producto_id>/", editar_productos, name="editar_productos"),
+    path("productos/eliminar/<int:id>", eliminar_productos, name="eliminar_productos")
 ]
 
--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Productos
 
 def index_productos(request):
-    return render(request, 'productos/index_productos.html')
+    productos = Productos.objects.all()
+    return render(request, 'productos/index_productos.html',{'productos':productos})
+
+def registrar_productos(request):
+    if request.method=='POST':
+        cod = request.POST.get('cod')
+        nombre = request.POST.get('nombre')
+        description = request.POST.get('description')
+        precio = request.POST.get('precio')
+        stock = request.POST.get('stock')
+
+        Productos.objects.create(
+            cod=cod,
+            nombre=nombre,
+            description=description,
+            precio=precio,
+            stock=stock
+        )
+
+        return redirect('index_productos')
+    return render(request,'productos/registrar_productos.html')
+
+def editar_productos(request, producto_id):
+    productos = get_object_or_404(Productos, id=producto_id)
+
+    if request.method =='POST':
+        productos.cod = request.POST.get('cod')
+        productos.nombre = request.POST.get('nombre')
+        productos.description = request.POST.get('description')
+        productos.precio = request.POST.get('precio')
+        productos.stock = request.POST.get('stock')
+        productos.save()
+        return redirect('index_productos')
+    return render(request, 'productos/editar_productos.html', {'productos':productos})
+
+def eliminar_productos(request, id):
+    if request.method == 'POST':
+        productos = Productos.objects.get(pk=id)
+        productos.delete()
+        return JsonResponse({'success':True})
